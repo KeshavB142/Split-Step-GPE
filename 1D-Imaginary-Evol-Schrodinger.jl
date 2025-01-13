fc60d496899706b14db5a571d67a5013be7b7c65
KeshavB142 <keshavbadri@gmail.com>
2025-01-13 01:28:27 -0500

    Incomplete Testing using 1D Infinite Well WaveFn

--- 1D-Imaginary-Evol-Schrodinger.jl
+++ 1D-Imaginary-Evol-Schrodinger.jl
@@ -1,16 +1,13 @@
 using FFTW
 using QuadGK
+using Plots
 
-m = 1 #Real line starting value
-δx = 10^(-5) #Grid spacing
+δx = 10^(-6) #Grid spacing
 t = 1 #Complete time evolution
-δt = 1/(10^-10) #Time step spacing
-function IC(n)
-    yArray = ones(n)
-    while m <= n*δx #Iterate until the max of the array length is reached with appropriate input values
-        yArray[m] = exp(-m) #Initial Condition Wavefunction
-        global m += δx
-    end
+δt = 10^(-4) #Time step spacing
+
+function IC(xArray :: Vector{Float64})
+    yArray = sqrt(2) .* sin.(π.*xArray) #Initial Condition Wavefunction
     return yArray #Numerical Array representing wavefunction
 end
 
@@ -18,7 +15,7 @@ function kineticOperatorStep(m, ψ, xArray) #Time Step Operation for the Kinetic
     kDomain = fft(ψ)
     N = length(ψ)
     k = (δx/N) .* xArray #Transforming the real line grid into the momentum space grid
-    weightApplication = exp.((-k.^2)./(2m) .* (δt / 2)) .* kDomain #Weighting each value in momentum space by the diagonalization factor
+    weightApplication = exp.((-k.^2)./(2*m) .* (δt / 2)) .* kDomain #Weighting each value in momentum space by the diagonalization factor
     xDomain = ifft(weightApplication)
     return xDomain
 end
@@ -34,27 +31,32 @@ function normalization(ψ) #Normalization of the Wavefunction (done discretely)
     return (ψ / L2Norm)
 end
 
-potentialArray = zeros(n) #Potential values at each point on the grid
+
+
 τ = δt #time step counter
-i = 1000 #Number of discrete values on the real line grid
-ψ = IC(i) #Initial condition
-mass = 9.1*(10^-31) #Particle mass
-xGrid = Array(0:δx:((i-1)*δx)) #Creates 1D discrete line of real space
-ϵ = 10^-15 #Convergence factor
+i = 1/(δx) #Number of discrete values on the real line grid
+mass = 1 #Particle mass
+
+m = 0 #Real line starting value
+xGrid = Array(m:δx:(m+((i-1)*δx))) #Creates 1D discrete line of real space
+ϵ = 10^-20 #Convergence factor
+
+potentialArray = zeros(Int64(i)) #Potential values at each point on the grid
+ψ = IC(xGrid) #Initial condition
+
 while τ <= t
     
     ψ0 = ψ
 
     #Split-Step Evolution
-    ψ = kineticOperatorStep(mass, ψ, xGrid)
+    global ψ = kineticOperatorStep(mass, ψ, xGrid)
     ψ = normalization(ψ)
     ψ = potentialOperatorStep(potentialArray, ψ)
     ψ = kineticOperatorStep(mass, ψ, xGrid)
     ψ = normalization(ψ)
 
-    if max(ψ - ψ0) < ϵ*δt
-        break
-    end
-    τ += δt
+    global τ += δt
 end
 
+
+plot(xGrid, real(ψ), label = "Re(ψ(x, t))", xlabel = "Position(x)", ylabel = "Wavefunction(ψ)")
