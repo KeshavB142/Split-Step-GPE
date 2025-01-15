a11e32d07d0c071582ac67bd40ecc2c9d0d5ab4a
KeshavB142 <keshavbadri@gmail.com>
2025-01-15 02:06:50 -0500

    K-Space Fixed, Test Success on HO and 1D Well, Minor Errors Likely due to Computing Limits

--- 1D-Imaginary-Evol-Schrodinger.jl
+++ 1D-Imaginary-Evol-Schrodinger.jl
@@ -1,24 +1,22 @@
 using FFTW
-using QuadGK
+using LinearAlgebra
 using Plots
 
 δx = 10^(-6) #Grid spacing (Most practical + accurate spacing on my machine)
-L = 1 #Interval Size
+L = 40 #Interval Size
 i = 10^6
 
-t = 1 #Complete time evolution
+mass = 1 #Particle mass
 δt = 10^(-2) #Time step spacing
 
-mass = 1 #Particle mass
+n = -20 #Real line starting value
+xGrid = LinRange(n, L+n, Int64(i)) #Creates 1D discrete line of real space
+k = LinRange(-π/(L), π/(L), Int64(i)) #Creates 1D discrete line of momentum space
 
-n = 0 #Real line starting value
-xGrid = LinRange(n, L, Int64(i)) #Creates 1D discrete line of real space
-k = π/(L) .* vcat(0:Int64((i))/2 - 1, -Int64((i))/2:-1) #Creates 1D discrete line of momentum space
-
-potentialArray = zeros(Int64(i)) #Potential values at each point on the grid
+potentialArray = .05 .* (xGrid).^2 #Potential values at each point on the grid
 
 function IC(xArray)
-    yArray = sqrt(2) .* sin.(π.* xArray) #Initial Condition Wavefunction
+    yArray = (1/(sqrt(10)*π))^(1/4) .* exp.(-((xArray.+2.5).^2)./(2*sqrt(10))) #Initial Condition Wavefunction
     return yArray #Numerical Array representing wavefunction
 end
 
@@ -30,7 +28,8 @@ function kineticOperatorStep(m, ψ1) #Time Step Operation for the Kinetic Operat
 end
 
 function potentialOperatorStep(V, ψ1) #Time Step Operation for the Potential Operator
-    operatedPsi = exp.(V .* δt ) .* ψ1 
+    X = -1 .* V .* δt
+    operatedPsi = exp.(X) .* ψ1 
     return operatedPsi
 end
 
@@ -39,30 +38,42 @@ function normalization(ψ1) #Normalization of the Wavefunction (done discretely)
     return (ψ1 ./ L2Norm)
 end
 
+function splitStepEvolution(time)
 
+    t = time #Complete time evolution
 
-τ = δt #time step counter
-ϵ = 10^-20 #Convergence factor
+    τ = δt #time step counter
+    ϵ = 10^-20 #Convergence factor
 
-ψ = IC(xGrid) #Initial condition Assignment
+    ψ = (IC(xGrid)) #Initial condition Assignment
 
-while τ <= t
+    while τ <= t
 
-    #Split-Step Evolution
-    global ψ = kineticOperatorStep(mass, ψ)
-    #print(maximum(real(ψ)) )
-    ψ = normalization(ψ)
-    #print(maximum(real(ψ)))
-    ψ = potentialOperatorStep(potentialArray, ψ)
-    #print(maximum(real(ψ)))
-    ψ = kineticOperatorStep(mass, ψ)
-    #print(maximum(real(ψ)))
-    ψ = normalization(ψ)
-    println(maximum(real(ψ)))
+        #Split-Step Evolution
+        ψ = kineticOperatorStep(mass, ψ)
+        #print(maximum(real(ψ)) )
+        ψ = normalization(ψ)
+        #print(maximum(real(ψ)))
+        ψ = potentialOperatorStep(potentialArray, ψ)
+        #print(maximum(real(ψ)))
+        ψ = kineticOperatorStep(mass, ψ)
+        #print(maximum(real(ψ)))
+        ψ = normalization(ψ)
+        #println(maximum(real(ψ)))
 
-    global τ += δt
+        τ += δt
+    end
+    return ψ
 end
 
+ψ2 = splitStepEvolution(10)
+ψ1 = splitStepEvolution(1)
+ψ3 = splitStepEvolution(5)
 
-plot(xGrid, real(ψ), label = "Re(ψ(x, t))", xlabel = "Position(x)", ylabel = "Wavefunction(ψ)")
-ylims!(minimum(real(ψ)), maximum(real(ψ)))
+
+plot(xGrid, real(ψ1), label = "1 Second", xlabel = "Position(x)", ylabel = "Wavefunction(ψ)")
+plot!(xGrid, potentialArray, label="Potential Well")
+plot!(xGrid, normalization(real(IC(xGrid))), label = "Initial Wavefunction")
+plot!(xGrid, real(ψ2), label = "10 Seconds")
+plot!(xGrid, real(ψ3), label = "5 Seconds")
+xlims!(xGrid[250000],xGrid[750000])
