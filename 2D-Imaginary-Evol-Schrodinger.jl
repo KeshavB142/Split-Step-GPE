bfb01b1efd00c55452f2d02c089d968b4f224260
KeshavB142 <keshavbadri@gmail.com>
2025-01-19 19:06:33 -0500

    Recommitting 2D SWE due to user error

--- 2D-Imaginary-Evol-Schrodinger.jl
+++ 2D-Imaginary-Evol-Schrodinger.jl
@@ -3,27 +3,31 @@ using LinearAlgebra
 using SparseArrays
 using Plots
 
-δx = 10^(-3) #Grid spacing (Most practical + accurate spacing on my machine)
-L = 1 #Interval Size
-i = 10^3
+δx = 10^(-2) #Grid spacing (Most practical + accurate spacing on my machine)
+L = 20 #Interval Size
+i = 10^2
 
 mass = 1 #Particle mass
 δt = 10^(-2) #Time step spacing
 
-n = 0 #Real line starting value
+n = -10 #Real line starting value
 xGrid = LinRange(n, L+n, Int64(i)) #Creates x-axis for the 3D space
 yGrid = LinRange(n, L+n, Int64(i)) #Creates y-axis for the 3D space
 Kx = LinRange(-π/(L), π/(L), Int64(i)) #Creates x-axis for the momentum space
 Ky = LinRange(-π/(L), π/(L), Int64(i)) #Creates y-axis for the momentum space
 
-potentialArray = spzeros(Int64(i), Int64(i)) #Potential values at each point on the grid
+potentialArray = spzeros(Int64(i), Int64(i))
+for i in eachindex(xGrid)
+    for j in eachindex(yGrid)
+        potentialArray[i,j] = 0.5 .* (xGrid[i]^2 + yGrid[j]^2) #Potential values at each point on the grid
+    end
+end
+potentialArray = Matrix(potentialArray)
 
 kNormGrid = spzeros(Int64(i), Int64(i))
-
 for i in eachindex(Kx)
     kNormGrid[i, :] = sqrt.(Kx[i]^2 .+ Ky.^2)
 end
-
 kNormGrid = Matrix(kNormGrid) #Norm of momentum space grid vectors
 
 Kx = Ky = nothing #Removing momentum space building vectors from memory
@@ -31,20 +35,22 @@ Kx = Ky = nothing #Removing momentum space building vectors from memory
 function IC(xArray, yArray)
     zArray = spzeros(Int64(i), Int64(i))
     for i in eachindex(xArray)
-        zArray[i,:] = 2 .*sin(π.*(xArray[i])) .* sin.(π.*yArray) #Infinite Square Well Wavefunction
+        for j in eachindex(yArray)
+            zArray[i,j] = (1/(π)) .* exp(-1 * ((xArray[i]-4.0)^2) / 2) .* exp.(-1*((yArray[j]-2.0)^2)/ 2) #Infinite Square Well Wavefunction
+        end
     end #Initial Condition Wavefunction
     return Matrix(zArray) #Numerical Array representing wavefunction
 end
 
 function kineticOperatorStep(m, ψ1) #Time Step Operation for the Kinetic Operator 
     weights = exp.(-1*((kNormGrid.^2)./(2*m)) .* (δt / 2)) #Calculation of the diagonalization factor
-    weightedψ = weights .* fftshift(fft(ψ1))  #Weighting each value in momentum space by the diagonalization factor
+    weightedψ = weights .* fft(ψ1)  #Weighting each value in momentum space by the diagonalization factor
     xDomain = ifft(weightedψ)
     return xDomain
 end
 
 function potentialOperatorStep(V, ψ1) #Time Step Operation for the Potential Operator
-    X = -1 .* norm(V) .* δt
+    X = -1 .* V .* δt
     operatedPsi = exp.(X) .* ψ1 
     return operatedPsi
 end
@@ -87,10 +93,10 @@ end
 ψ3 = splitStepEvolution(5)
 
 plot(
-    surface(xGrid, yGrid, real(ψ1), title = "1 Second", xlabel = "Position(x)", ylabel = "Position (y)", zlabel = "Wavefunction(ψ)", colorbar=false),
-#plot!(xGrid, potentialArray, label="Potential Well")
+    surface(xGrid, yGrid, potentialArray, title="Potential Well", colorbar=false),
     surface(xGrid, yGrid, normalization(real(IC(xGrid, yGrid))), title = "Initial Wavefunction", colorbar=false),
+    surface(xGrid, yGrid, real(ψ1), title = "1 Second", xlabel = "Position(x)", ylabel = "Position (y)", zlabel = "Wavefunction(ψ)", colorbar=false),
     surface(xGrid, yGrid, real(ψ2), title = "3 Seconds", colorbar=false),
-    surface(xGrid, yGrid, real(ψ3), title = "5 Seconds", colorbar=false)
+    surface(xGrid, yGrid, real(ψ3), title = "5 Seconds", colorbar=false),
 #xlims!(xGrid[250000],xGrid[750000])
 )
