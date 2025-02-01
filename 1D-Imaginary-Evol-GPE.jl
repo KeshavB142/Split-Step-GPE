dd1f4a1f0a02f9acc6fcd31a400e9c378d3d0cac
KeshavB142 <keshavbadri@gmail.com>
2025-02-01 00:15:34 -0500

    Completed (for now) 1D GPE

--- 1D-Imaginary-Evol-GPE.jl
+++ 1D-Imaginary-Evol-GPE.jl
@@ -2,24 +2,24 @@ using FFTW
 using LinearAlgebra
 using Plots
 
-δx = 10^(-4) #Grid spacing (Most practical + accurate spacing on my machine)
-L = 30 #Interval Size
+δx = 10^(-3) #Grid spacing (Most practical + accurate spacing on my machine)
+L = 200 #Interval Size
 i = Int64(round(1 / δx))
 
 mass = 1 #Particle mass
 δt = 10^(-2) #Time step spacing
 j = Int64(round(1 / δt))
 
-n = -15 #Real line starting value
+n = -100 #Real line starting value
 xGrid = LinRange(n, L+n, i) #Creates 1D discrete line of real space
 k = LinRange(-π/(L), π/(L), i) #Creates 1D discrete line of momentum space
 
-potentialArray = .5 .* ((xGrid).^2) #Potential values at each point on the grid
+potentialArray = 1 .* ((xGrid).^2) #Potential values at each point on the grid
 N = 10^3 #Number of particles
-g = 10 #Strength of interaction term
+g = 1.5 #Strength of interaction term
 
 function IC(xArray)
-    yArray = exp.((-(xArray .- 3).^2)) #Initial Condition Wavefunction
+    yArray = exp.((-(xArray .- 0).^2)/20) #Initial Condition Wavefunction
     return yArray #Numerical Array representing wavefunction
 end
 
@@ -50,7 +50,7 @@ function calculatedChemPotential(ψ1)
     return sqrt(sum(abs2.(KE+PE+IE) * δx))
 end
 
-μArray = zeros((360*j))
+μArray = zeros((60*j))
 μArray[1] = calculatedChemPotential(sqrt(N) .* normalization(IC(xGrid)))
 function splitStepEvolution(time)
 
@@ -60,19 +60,21 @@ function splitStepEvolution(time)
     #Split Step Evolution
     for i = 1:Nmax
         ψ = sqrt(N).*normalization(kineticOperatorStep(mass,potentialOperatorStep(potentialArray, normalization(kineticOperatorStep(mass, ψ)))))
-        μArray[i] = calculatedEnergy(ψ)
+        μArray[i] = calculatedChemPotential(ψ)
     end
     return ψ
 end
 
-ψout= [splitStepEvolution(i) for i in 0:45:360] #Time period evolved Wavefns
-timeLabelArray = ["t="*string(i)*"s" for i in 0:45:360] #Label array for time lines
+ψout= [splitStepEvolution(i) for i in 0:3:60] #Time period evolved Wavefns
+timeLabelArray = ["t="*string(i)*"s" for i in 0:3:60] #Label array for time lines
 
-mag = ψout[8] .* conj(ψout[8])
-mag1 = ψout[1] .* conj(ψout[1])
-plot(xGrid, potentialArray, title = "Wavefunction vs. Position", label="Potential Well")
-plot!(xGrid, real(mag))
-#plot!(xGrid, real(mag1))
+plt = plot()
+plot!(plt, xGrid, potentialArray, title = "|ψ|^2 vs. Position", label="Potential Well")
+for i = 1:21
+    local mag = (ψout[i]) .* conj((ψout[i]))
+    plot!(plt, xGrid, real(mag), label = string(timeLabelArray[i]))
+end
+current()
 
-#label = timeLabelArray[i]
-#plot(1:5*j+1, μArray, title="μ Evolution", xlabel="Time(s)", label=false)
+#, label=string(timeLabelArray[i])
+#plot(1:15*j, μArray, title="μ Evolution", xlabel="Time(s)", label=false)
